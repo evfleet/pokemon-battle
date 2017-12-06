63f84f10b976dbeffb733f145cb974b22173c43c
Evan Fleet <evfleet@gmail.com>
2017-12-06 12:49:26 -0500

    Battle functionality implemented

--- program-babel.js
+++ program-babel.js
@@ -11,7 +11,7 @@ function fetchPokemon(identifier) {
     const [ firstInput, secondInput ] = process.argv.slice(2);
 
     if (!(firstInput && secondInput)) {
-
+      throw new Error('Two pokemon are required to battle');
     }
 
     const [ firstPokemon, secondPokemon ] = await Promise.all([
@@ -19,13 +19,25 @@ function fetchPokemon(identifier) {
       fetchPokemon(secondInput)
     ]);
 
+    if (firstPokemon.detail === 'Not found.' || secondPokemon.detail === 'Not found.') {
+      throw new Error('Pokemon not found');
+    }
+
     const winner = battle(firstPokemon, secondPokemon);
 
     console.log(winner);
 
     process.exit();
   } catch (error) {
-    console.log(error);
+    switch (error.message) {
+      case 'Two pokemon are required to battle':
+      case 'Pokemon not found':
+        console.log(error.message);
+        break;
+      default:
+        console.log('Unexpected error');
+        break;
+    }
 
     process.exit();
   }
